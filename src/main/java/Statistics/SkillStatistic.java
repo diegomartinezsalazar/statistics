859f37c6b61a8bb90bacff95108a4bba7046c3e1
diegomartinezsalazar <diegovol@hotmail.com>
2018-04-05 23:56:35 +0200

    Termino la lectura de un partido completo y muestro el resultado al final

---
+++ src/main/java/Statistics/SkillStatistic.java
@@ -0,0 +1,18 @@
+package Statistics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SkillStatistic {
+    private String tipo = new String();
+    private Map<String, Integer> lista = new HashMap<>();
+
+    public SkillStatistic(){
+
+    }
+
+    public void addSkill(String key){
+        int skillNumber = lista.get(key);
+
+    }
+}
