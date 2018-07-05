cb4f555838c3b3dd90bb60d1f03bc56f0c7fd1a0
dimartinez <dimartinez@homeaway.com>
2018-07-05 10:05:26 +0200

    Clase que comprueba si el comienzo de una jugada es correcto

--- src/main/java/com/mycompany/sportstats/TeamUtils/Tester/JugadaTester.java
+++ src/main/java/com/mycompany/sportstats/TeamUtils/Tester/JugadaTester.java
@@ -1,17 +1,18 @@
-package com.mycompany.sportstats.Team.Match.Tester;
+package com.mycompany.sportstats.TeamUtils.Tester;
 
 import java.util.List;
 
 import com.mycompany.sportstats.Team.Match.ErrorContrario;
 import com.mycompany.sportstats.Team.Match.Jugada;
-import com.mycompany.sportstats.Team.Match.Movement;
 import com.mycompany.sportstats.Team.Match.PuntoContrario;
 import com.mycompany.sportstats.Team.Match.Skill.Recepcion;
 import com.mycompany.sportstats.Team.Match.Skill.Saque;
+import com.mycompany.sportstats.Team.Match.Skill.Skill;
 import com.mycompany.sportstats.Team.Match.Tiempo;
 
 public class JugadaTester {
     private List<Class> jugadaStart;
+    private List<Class> jugadaEnd;
 
     public JugadaTester (){
         jugadaStart.add(Recepcion.class);
@@ -19,13 +20,29 @@ public class JugadaTester {
         jugadaStart.add(ErrorContrario.class);
         jugadaStart.add(PuntoContrario.class);
         jugadaStart.add(Tiempo.class);
+
+        jugadaEnd.add(ErrorContrario.class);
+        jugadaEnd.add(PuntoContrario.class);
+        jugadaEnd.add(Tiempo.class);
     }
 
     public boolean testJugadaStart (Jugada jugada){
         return isJugadaStart(jugada.getMovimientos().get(0));
     }
 
-    private boolean isJugadaStart(Object jugada){
-        return jugadaStart.contains(jugada.getClass());
+    private boolean isJugadaStart(Object movimiento){
+        return jugadaStart.contains(movimiento.getClass());
+    }
+
+    private boolean isJugadaEnd(Object movimiento){
+        return jugadaEnd.contains(movimiento.getClass()) || movimiento;
+    }
+
+    private boolean isFinishMovement (Object mov){
+        if (jugadaEnd.contains(mov.getClass())){
+
+        } else if (mov instanceof Skill){
+
+        }
     }
 }
